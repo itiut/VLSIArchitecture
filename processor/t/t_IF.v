e4339394d160cf87b22d3478cef43ad5e9f37913
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2013-07-04 01:38:53 +0900

    Pass IF unit test

--- processor/t/t_IF.v
+++ processor/t/t_IF.v
@@ -1,28 +1,41 @@
 module t_IF;
     reg clk;
     reg n_rst;
+    reg stall;
+    reg [31:0] pc_branched;
+    reg do_branch;
     wire [31:0] pc;
     wire [31:0] ir;
-    integer i;
 
     initial begin
-        clk = 0;
-        for (i = 0; i < 10; i = i + 1) begin
-            #100 clk = ~clk;
-        end
+        clk <= 0;
+        n_rst <= 1;
+        stall <= 0;
+        pc_branched <= 0;
+        do_branch <= 0;
+
+        #10 n_rst <= 0;
+        #10 n_rst <= 1;
+
+        #200 stall <= 1;
+        #400 stall <= 0;
+
+        #200 do_branch <= 1; pc_branched <= 100;
+        #100 do_branch <= 0;
+
+        #200 do_branch <= 1; pc_branched <= 0;
+        #100 do_branch <= 0;
+
+        #1000 $stop;
     end
 
-    initial begin
-        n_rst = 1;
-        #20 n_rst = 0;
-        #20 n_rst = 1;
-    end
+    always #50 clk = ~clk;
 
     IF IF(.clk_i(clk),
           .n_rst_i(n_rst),
-          .ID_stall_i(1'b0),
-          .MEM_pc_branched_i(32'b0),
-          .MEM_do_branch_i(1'b0),
+          .ID_stall_i(stall),
+          .MEM_pc_branched_i(pc_branched),
+          .MEM_do_branch_i(do_branch),
           .IFID_pc_o(pc),
           .IFID_ir_o(ir));
 
